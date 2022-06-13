a326c782b22970299da9e1a404571dda26fa2464
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-06-13 14:15:28 +0900

    ws server create

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,8 +7,14 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+//유저들이 /public 으로 이동할 때 public 폴더 내용을 볼 수 있음.
 
+//catchall url
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
